ce741db9aa83ca1c28e5b44f78376be5b2cd9a0f
Pedro Philipe <pedrophilipe183@gmail.com>
2026-04-02 19:30:07 -0300

    Projeto GoLang

---
+++ SRC/HELLO/hello.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	exibeIntroducao()
+	exibeMenu()
+
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		iniciarMonitoramento()
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 3:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Esse comando não existe")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
+	nome := "Pedro"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome, "sua idade é")
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("3 - Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	// site com URL inexistente
+	site := "https://www.youtube.com" // ou 200
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
+}
